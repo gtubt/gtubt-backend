079c8c109424b89eb9bcb36887cd37bd78afda3f
Mehmet Gürol Çay <m.gurolcay@gmail.com>
2021-08-29 22:24:13 +0300

    fix register serializer

--- app/users/models.py
+++ app/users/models.py
@@ -7,7 +7,7 @@ from core.utils.models import StarterModel
 
 class User(AbstractUser, StarterModel):
     username = models.CharField(default="", max_length=255)
-    name = models.CharField("First Name", max_length=255)
+    first_name = models.CharField("First Name", max_length=255)
     last_name = models.CharField("Last Name", max_length=255)
     department = models.CharField(
         max_length=3, choices=Department.choices, default=Department.cse
@@ -18,6 +18,8 @@ class User(AbstractUser, StarterModel):
     photo = models.ImageField(blank=True, null=True)
     phone = models.CharField("Phone Number", max_length=128)
     is_active = models.BooleanField("Active", default=True)
+    is_accept_kvkk = models.BooleanField("KVKK", default=False)
+    is_accept_user_agreement = models.BooleanField('User Agreement', default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
--- app/users/resources/serializers.py
+++ app/users/resources/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "email",
-            "name",
+            "first_name",
             "last_name",
             "department",
             "year",
@@ -24,8 +24,38 @@ class UserSerializer(serializers.ModelSerializer):
             "phone",
             "is_active",
             "password",
+            "is_accept_kvkk",
+            "is_accept_user_agreement"
         ]
 
 
 class RegisterSerializer(DefaultRegisterSerializer):
-    username = serializers.CharField(max_length=255, default="", required=False)
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    student_id = serializers.CharField()
+    is_accept_kvkk = serializers.BooleanField()
+    is_accept_user_agreement = serializers.BooleanField()
+
+    def get_cleaned_data(self):
+        return {
+            "username": self.validated_data.get("username", ""),
+            "password1": self.validated_data.get("password1", ""),
+            "email": self.validated_data.get("email", ""),
+            "first_name": self.validated_data.get("first_name", ""),
+            "last_name": self.validated_data.get("last_name", ""),
+        }
+
+    def save(self, request):
+        user = super(RegisterSerializer, self).save(request)
+        data = {
+            "student_id": self.validated_data.get("student_id", ""),
+            "is_accept_kvkk": self.validated_data.get("is_accept_kvkk", False),
+            "is_accept_user_agreement": self.validated_data.get(
+                "is_accept_user_agreement", False
+            ),
+        }
+
+        for key, value in data.items():
+            setattr(user, key, value)
+        user.save(update_fields=data.keys())
+        return user
