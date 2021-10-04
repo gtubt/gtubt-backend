90378c0113e030cef19f1fca713110e266a9ba7e
Ahmet Ergani <ahmet.ergani05@gmail.com>
2021-10-04 22:00:48 +0300

    Sentry Integration
    Added sentry integration

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import sentry_sdk
 from pathlib import Path
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -179,3 +181,19 @@ try:
     from .settings_local import *
 except ImportError:
     pass
+
+SENTRY_KEY = os.getenv("SENTRY_KEY")
+
+sentry_sdk.init(
+    dsn=SENTRY_KEY,
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
