08feb9dcd6f3290406931bd0ac3f745f9fa78472
Mehmet Gürol Çay <m.gurolcay@gmail.com>
2021-10-24 23:59:59 +0300

    add password reset confirm serializer

--- app/auth/resources/serializers.py
+++ app/auth/resources/serializers.py
@@ -1,7 +1,13 @@
 from dj_rest_auth.serializers import \
+    PasswordResetConfirmSerializer as DefaultPasswordResetConfirmSerializer
+from dj_rest_auth.serializers import \
     PasswordResetSerializer as DefaultPasswordResetSerializer
+from django.conf import settings
 from django.contrib.auth.forms import PasswordResetForm
 from django.db.models import Q
+from django.utils.encoding import force_str
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from app.auth.exceptions import AuthUserInactiveException
 from app.users.models import User
@@ -28,3 +34,45 @@ class PasswordResetSerializer(DefaultPasswordResetSerializer):
         else:
             raise AuthUserInactiveException()
         return super(PasswordResetSerializer, self).validate_email(value)
+
+    def save(self):
+        from django.contrib.auth.tokens import default_token_generator
+
+        request = self.context.get('request')
+        # Set some values to trigger the send_email method.
+        opts = {
+            'use_https': request.is_secure(),
+            'from_email': getattr(settings, 'DEFAULT_FROM_EMAIL'),
+            'request': request,
+            'token_generator': default_token_generator,
+        }
+
+        opts.update(self.get_email_options())
+        self.reset_form.save(**opts)
+
+
+class PasswordResetConfirmSerializer(DefaultPasswordResetConfirmSerializer):
+    def validate(self, attrs):
+        from django.contrib.auth.tokens import default_token_generator
+        from django.utils.http import urlsafe_base64_decode as uid_decoder
+
+        # Decode the uidb64 (allauth use base36) to uid to get User object
+
+        try:
+            uid = force_str(uid_decoder(attrs['uid']))
+            self.user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            raise ValidationError({'uid': ['Invalid value']})
+
+        if not default_token_generator.check_token(self.user, attrs['token']):
+            raise ValidationError({'token': ['Invalid value']})
+
+        self.custom_validation(attrs)
+        # Construct SetPasswordForm instance
+        self.set_password_form = self.set_password_form_class(
+            user=self.user, data=attrs,
+        )
+        if not self.set_password_form.is_valid():
+            raise serializers.ValidationError(self.set_password_form.errors)
+
+        return attrs
